cdfbaba6ca708727da1e4f8240b061847c4f3c86
MorganeFar <131769763+MorganeFar@users.noreply.github.com>
2024-12-05 22:11:56 +0100

    feat : so damn terrible input

--- index2.js
+++ index2.js
@@ -1,5 +1,13 @@
 let body = document.getElementsByTagName("body")
+let input = document.getElementsByTagName("form")
 
+console.log(input)
+
+// Title action
 let title = document.title
 window.addEventListener("blur", (event)=>document.title = "Reviens, je meurs !")
 window.addEventListener("focus", (event)=>document.title = "Sauvons l'océan")
+
+// Input action
+let firstname = document.getElementById('firstname')
+firstname.addEventListener("input", ()=>firstname.value = firstname.value.split("").reverse().join(""))
