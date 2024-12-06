0b03612de2ab98d25c0491e0aeb3ec60e2adb4f5
MorganeFar <131769763+MorganeFar@users.noreply.github.com>
2024-12-06 02:06:36 +0100

    feat : some bubbles popping randomly

--- index2.js
+++ index2.js
@@ -1,4 +1,5 @@
-let body = document.getElementsByTagName("body")
+let body = document.querySelector("body")
+console.log(body)
 
 // Title action
 let title = document.title
@@ -17,3 +18,41 @@ lastname.addEventListener("input", ()=>{
     });
     lastname.value = tab_temp.join("") 
 })
+
+// Bubble creation
+let score = document.querySelector(".score")
+let scoreNum = 0
+
+setInterval(()=>{
+    let bubble = document.createElement("div")
+    bubble.classList.add("bubble")
+    body.appendChild(bubble)
+
+    //position 
+    bubble.style.top = (10 +Math.random()*70)+"vh"
+    bubble.style.left = (10+ Math.random()*70)+"vw"
+
+    let r =Math.round(Math.random()*255)
+    let g =Math.round(Math.random()*255)
+    let b =Math.round(Math.random()*255)
+    let color = "rgba(132,159,0255,0.8)"
+    let colorBorder = "rgba("+r+","+g+","+b+ ","+0.7+")"
+    bubble.style.background = color
+    bubble.style.border = colorBorder
+
+    bubble.addEventListener("mouseenter", function(){
+        bubble.classList.add("bigBubble")
+    })
+
+    bubble.addEventListener("mouseleave", function(){
+        bubble.classList.remove("bigBubble")
+    })
+
+    bubble.addEventListener("click", function(){
+        scoreNum +=1
+        bubble.remove()
+        score.textContent = scoreNum
+        
+    })
+}
+, 1000)
